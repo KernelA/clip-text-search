e5ee01de9d449f80f0c0efb1db9f1d01b37b8999
KernelA <17554646+KernelA@users.noreply.github.com>
2022-01-09 17:18:57 +0300

    remove entry point

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip -O Flickr8k_Dataset.zip
-
-unzip ./Flickr8k_Dataset.zip Flicker8k_Dataset/* && rm ./Flickr8k_Dataset.zip
-
-exec "$@"
